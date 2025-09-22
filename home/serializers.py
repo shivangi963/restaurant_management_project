b4a220e424c3935171a3902c98fec40aff109cbd
shivangi963 <shuklashivangi963@gmail.com>
2025-09-22 18:55:07 +0530

    49654

--- home/serializers.py
+++ home/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import MenuCategory
+
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuCategory
+        fields=['id','name']
