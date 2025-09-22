65b3b0a76c5482c63d4a26396dbd5f94412e54c7
shivangi963 <shuklashivangi963@gmail.com>
2025-09-23 00:17:55 +0530

    49651

--- orders/models.py
+++ orders/models.py
@@ -7,3 +7,12 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.code
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {customer_name}"         
