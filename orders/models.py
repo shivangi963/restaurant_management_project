c1e34f49e1231d86ac5d090ee7b7d6cb301af928
shivangi963 <shuklashivangi963@gmail.com>
2025-09-23 00:04:08 +0530

    49652

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Coupon(models.Model):
-    code=models.CharField(max_length=20, unique=True)
-    discount= models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+class OrderStatus(models.Model):
+    name=models.CharField(max_length=50, unique=True)
+
 
     def __str__(self):
         return self.code
